48eb30034b88f251b2d63fb2ab4950f034eecfa9
Andrew Fillenwarth <andyf016@gmail.com>
2021-01-26 17:20:48 -0500

    setting up sequence inputs and outputs

--- dna-app.py
+++ dna-app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import altair as alt
 from PIL import Image
 
-### Page title
+# Page title
 
 image = Image.open('dna-logo.jpg')
 
@@ -14,3 +14,23 @@ DNA Nucleotide Count Web App
 
 This app counts the nucleotide composition of query DNA
 """)
+
+# Input text box
+
+st.header('Enter DNA sequence')
+
+sequence_input = ">DNA Query 2\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGTGCTCGGG\nATCTTCCAGACGTCGCGACTCTAAATTGCCCCCTCTGAGGTCAAGGAACACAAGATGGTTTTGGAAATGC\nTGAACCCGATACATTATAACATCACCAGCATCGTGCCTGAAGCCATGCCTGCTGCCACCATGCCAGTCCT"
+sequence = st.text_area("Sequence input", sequence_input, height=250)
+sequence = sequence[1:] # skips the first line (sequence name)
+sequence = ''.join(sequence) #Concats list into string
+
+st.write("""
+***
+""")
+
+# Prints the input DNA sequence
+st.header('INPUT (DNA Query)')
+sequence
+
+# DNA nucleotide count
+st.header('OUTPUT (DNA Nucleotide Count)')
