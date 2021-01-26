653f62bb7a36bc49b0ffee74fb23b912bf1b776f
Andrew Fillenwarth <andyf016@gmail.com>
2021-01-26 17:12:56 -0500

    start dna count app

---
+++ dna-app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import streamlit as st
+import altair as alt
+from PIL import Image
+
+### Page title
+
+image = Image.open('dna-logo.jpg')
+
+st.image(image, use_column_width=True)
+
+st.write("""
+DNA Nucleotide Count Web App
+
+This app counts the nucleotide composition of query DNA
+""")
