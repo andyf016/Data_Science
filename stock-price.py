d45f35d11532175a33d30e64b39040cb0d001e0f
Andrew Fillenwarth <andyf016@gmail.com>
2021-01-25 23:36:23 -0500

    complete scaffolding

--- stock-price.py
+++ stock-price.py
@@ -7,3 +7,12 @@ Simple Stock price app
 
 Shown are the stock closing price and volume of Google
 """)
+# Define ticker symbol
+tickerSymbol = 'GOOGL'
+# Get data about ticker 
+tickerData = yf.Ticker(tickerSymbol)
+# Get historical prices for this ticker
+tickerDF = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+
+st.line_chart(tickerDF.Close)
+st.line_chart(ticker.Volume)
