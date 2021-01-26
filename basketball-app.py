fb9cab4da34e811e499e123e087f71f53ef95382
Andrew Fillenwarth <andyf016@gmail.com>
2021-01-26 18:44:27 -0500

    initial scaffolding

--- basketball-app.py
+++ basketball-app.py
@@ -6,3 +6,12 @@ import seaborn as sns
 import numpy as np
 
 st.title('NBA Player Stats Explorer')
+
+st.markdown("""
+This app performs simple web scraping of NBA player stats data
+* **Python libraries:** base64, pandas, streamlit
+* **Data source:** [Basketball-refrence.com](https://www.basketball-refrence.com/).
+""")
+
+st.sidebar.header('User Input Features')
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950, 2020))))
